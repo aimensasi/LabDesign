6bd21d9699010914f57f216c001a2ce6e0c1efe0
AIMEN.S.A.SASI <ali.sasi@ymail.com>
2016-06-16 00:53:57 +0800

    Gulp.js is ready

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyHtml = require('gulp-minify-html'),
     imagemin = require('gulp-imagemin'),
-    pngCruch = require('imagemin-pngcrush');
+    pngCrush = require('imagemin-pngcrush');
 
 var env,
     outputDir,
@@ -36,7 +36,7 @@ var env,
     imagesSrc = 'builds/developments/images/*.*';
 
 gulp.task('compass', function(){
-    gulp.src(style)
+    gulp.src(styleSrc)
         .pipe(compass({
             sass: 'components/sass',
             image: outputDir + 'images',
@@ -47,11 +47,48 @@ gulp.task('compass', function(){
         .pipe(connect.reload())
 });
 
+gulp.task('js', function(){
+    gulp.src(jsSrc)
+        .pipe(concat('script.js'))
+        .pipe(browerify())
+        .pipe(gulpIf(env === 'production', uglify()))
+        .pipe(gulp.dest(outputDir + 'js'))
+        .pipe(connect.reload())
+});
 
+gulp.task('html', function(){
+    gulp.src(htmlSrc)
+        .pipe(gulpIf(env === 'production', minifyHtml()))
+        .pipe(gulpIf(env === 'production', gulp.dest(outputDir)))
+        .pipe(connect.reload())
+});
 
+gulp.task('images', function(){
+    gulp.src(imagesSrc)
+        .pipe(gulpIf(env === 'production', imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngCrush()]
+        })))
+        .pipe(gulpIf(env === 'production', gulp.dest(outputDir + 'images')))
+        .pipe(connect.reload())
+});
 
+gulp.task('connect', function(){
+    connect.server({
+        root: outputDir,
+        livereload: true
+    })
+});
 
+gulp.task('watch', function(){
+    gulp.watch(sassSrc, ['compass']);
+    gulp.watch(jsSrc, ['js']);
+    gulp.watch(htmlSrc, ['html']);
+    gulp.watch(imagesSrc, ['images']);
+});
 
+gulp.task('default', ['html', 'js', 'compass', 'images', 'connect', 'watch']);
 
 
 
